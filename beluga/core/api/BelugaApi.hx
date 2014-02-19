23cc1ffd999cc0166c3caa416a86dc62450e5c12
Masadow <jimmy.delas@gmail.com>
2014-02-19 10:22:09 +0000

    API generation:

--- beluga/core/api/BelugaApi.hx
+++ beluga/core/api/BelugaApi.hx
@@ -1,27 +1,27 @@
-package beluga.core;
+package beluga.core.api;
 
 import haxe.web.Dispatch;
 import beluga.module.account.Account;
 import beluga.module.account.api.AccountApi;
 
-class BelugaApi 
+class BelugaApi implements IAPI
 {
 	private var beluga : Beluga;
-	private var acc : Account;
 
 	public function new(beluga : Beluga) {
 		this.beluga = beluga;
-		this.acc = beluga.getModuleInstance(Account);
 	}
 
 	//Handle url like www.beluga.fr?trigger=login
-	public function doDefault() {
+	public function doDefault(d : Dispatch) {
 		Sys.print("Vous etes sur la page d'accueil !");
 	}
-
-	//Handle url like www.beluga.fr/account/login
-	public function doAccount(d : Dispatch) {
-		d.dispatch(new AccountApi(beluga, acc));
-	}
+	
+	/*
+	 * Modules API are generated like:
+		 * public function doModule(d : Dispatch) {
+			* d.dispatch(new ModuleAPI(beluga));
+		 * }
+	 */
 
 }
