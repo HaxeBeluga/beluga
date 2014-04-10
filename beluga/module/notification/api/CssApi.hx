6d7593e0ecaf6d89fe0acd97b033a27834ed58ae
imperio06 <guillaume1.gomez@epitech.eu>
2014-04-10 01:42:47 +0100

    Creation of a new module : the notification module

---
+++ beluga/module/notification/api/CssApi.hx
@@ -0,0 +1,20 @@
+package beluga.module.notification.api;
+
+import php.Web;
+import haxe.Resource;
+import beluga.core.Beluga;
+import beluga.module.notification.Notification;
+
+class CssApi
+{
+
+	public function new() 
+	{
+	}
+
+	public function doPrint() {
+		Web.setHeader("Content-type", "text/css");
+		Sys.print(Beluga.getInstance().getModuleInstance(Notification).getWidget("print").getCss());
+	}
+
+}
