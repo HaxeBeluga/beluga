cfbce6b346262f6cf475e05bfe5277616caa4634
regnarock <thomas.wilgenbus@gmail.com>
2014-05-10 22:04:57 +0900

    MetadataLoader finished.

--- beluga/core/macro/MetadataLoader.hx
+++ beluga/core/macro/MetadataLoader.hx
@@ -1,26 +1,37 @@
 package beluga.core.macro;
 
 import beluga.core.macro.ConfigLoader;
-
+import haxe.macro.Expr;
+import haxe.macro.Expr.ExprOf;
+import haxe.macro.Context;
 /**
  * ...
  * @author regnarock
  */
-class MetadataLoader
+#if macro
+
+typedef MetadataProperties = {params : Array<String>, clazz : String, method : String};
+
+class MetadataLoader implements Dynamic<Array<MetadataProperties>>
 {
-	private static var triggersRoute : Array<{trigger : String, clazz : Dynamic, method : String}> = null;
-	
-	macro public static function getMetadata() {
-		if (triggersRoute != null)
-			return macro triggersRoute;
-		return macro readMetadata();
+	private static var metadatas : Map < String, Array<MetadataProperties> > = null;
+	public static var metadata = new MetadataLoader();
+
+	public function new ()
+	{
 	}
 
-	macro public static function readMetadata() {
+	public function resolve( name : String )
+	{
+		if (metadatas == null)
+			readMetadata();
+		return metadatas.get(name);
+	}
+	
+	private static function readMetadata() {
 		if (!ConfigLoader.isReady)
 			ConfigLoader.forceBuild();
-		triggersRoute = [];
-		
+		metadatas = new Map<String, Array<MetadataProperties> >();
 		for (triggerClass in ConfigLoader.config.nodes.loadmetadata) {
 			var clazz = triggerClass.att.resolve("class");
 			
@@ -34,16 +45,19 @@ class MetadataLoader
 						case FMethod(_):
 							// for each metada of the method
 							for (metadataEntry in field.meta.get()) {
-								switch (metadataEntry.name)
-								{
-									case "trigger":
-										switch (metadataEntry.params[0])
-										{
-											case {expr: EConst(CString(trigger)), pos: _}:
-												triggersRoute.push({trigger : trigger, clazz : clazz, method : field.name});
-											default:
-										}
-									default:
+								var args : Array<String> = [];
+								for (param in metadataEntry.params) {
+									switch (param) {
+										case { expr: EConst(CString(param)), pos: _ } :
+											args.push(param);
+										default:
+									}
+								}
+								var newEntry = {params : args, clazz : clazz, method : field.name} ;
+								if (metadatas.exists(metadataEntry.name)) {
+									metadatas.get(metadataEntry.name).push(newEntry);
+								} else {
+									metadatas.set(metadataEntry.name, [newEntry]);
 								}
 							}
 						default:
@@ -51,8 +65,10 @@ class MetadataLoader
 					}
 				default:
 			}
+			
 		}
-		
-		return Context.makeExpr(triggersRoute, Context.currentPos());
+
 	}
 }
+
+#end
