4c422c38f0c088da923ca541d86692a19cdcdd8c
GuillaumeGomez <guillaume1.gomez@gmail.com>
2014-09-08 17:52:02 +0200

    Clean source code of Notification module

--- test/src/modules/notification_test/NotificationTest.hx
+++ test/src/modules/notification_test/NotificationTest.hx
@@ -20,79 +20,39 @@ import neko.Web;
 class NotificationTest {
     public var beluga(default, null) : Beluga;
     public var notif(default, null) : Notification;
-    private var error_msg : String;
-    private var success_msg : String;
 
     public function new(beluga : Beluga) {
         this.beluga = beluga;
         this.notif = beluga.getModuleInstance(Notification);
         this.notif.triggers.defaultNotification.add(this.doDefault);
-        this.notif.triggers.createFail.add(this.doCreateFail);
-        this.notif.triggers.createSuccess.add(this.doCreateSuccess);
-        this.notif.triggers.deleteFail.add(this.doDeleteFail);
-        this.notif.triggers.deleteSuccess.add(this.doDeleteSuccess);
+        this.notif.triggers.createFail.add(this.doDefault);
+        //this.notif.triggers.createSuccess.add(this.doCreateSuccess);
+        this.notif.triggers.deleteFail.add(this.doDefault);
+        this.notif.triggers.deleteSuccess.add(this.doDefault);
         this.notif.triggers.print.add(this.doPrint);
-        this.error_msg = "";
-        this.success_msg = "";
     }
 
     public function doDefault() {
-        var user = Beluga.getInstance().getModuleInstance(Account).loggedUser;
-        if (user == null) {
-            Web.setHeader("Content-Type", "text/plain");
-            Sys.println("Please log in !");
-            return;
-        }
         var widget = notif.getWidget("notification");
-        widget.context = {notifs : notif.getNotifications(), user : user, error : error_msg, success : success_msg, path : "/beluga/notification/"};
 
-        var notifWidget = widget.render();
-
+        widget.context = notif.getDefaultContext();
         var html = Renderer.renderDefault("page_notification", "Notifications list", {
-            notificationWidget: notifWidget
+            notificationWidget: widget.render()
         });
         Sys.print(html);
     }
 
-    public function doPrint(args : {notif : NotificationModel}) {
-        var user = Beluga.getInstance().getModuleInstance(Account).loggedUser;
-        if (user == null) {
+    public function doPrint(args : {notif_id : Int}) {
+        if (notif.canPrint(args.notif_id)) {
+            var widget = notif.getWidget("print_notif");
+
+            widget.context = notif.getPrintContext(args.notif_id);
+            var html = Renderer.renderDefault("page_notification", "Notification", {
+                notificationWidget: widget.render()
+            });
+            Sys.print(html);
+        } else {
             doDefault();
-            return;
         }
-        if (args.notif == null) {
-            error_msg = "Notification hasn't been found...";
-            doDefault();
-            return;
-        }
-        var widget = notif.getWidget("print_notif");
-        widget.context = {notif : args.notif, path : "/beluga/notification/"};
-
-        var notifWidget = widget.render();
-
-        var html = Renderer.renderDefault("page_notification", "Notification", {
-            notificationWidget: notifWidget
-        });
-        Sys.print(html);
-    }
-
-    public function doCreateFail() {
-        error_msg = "Error ! Notification has not been created...";
-        this.doDefault();
-    }
-
-    public function doCreateSuccess() {
-        // success_msg = "Notification has been successfully created !";
-        // this.doDefault();
-    }
-
-    public function doDeleteSuccess() {
-        success_msg = "Notification has been successfully deleted !";
-        this.doDefault();
-    }
-
-    public function doDeleteFail() {
-        error_msg = "Error when trying to delete notification...";
-        this.doDefault();
     }
 }
