d940ffe8fc55ce8187675c1c2dab8185c60d4fa2
regnarock <thomas.wilgenbus@gmail.com>
2014-09-10 16:03:30 +0200

    Corrected many bugs. Still need to be tested.

--- src/ModuleFactory.hx
+++ src/ModuleFactory.hx
@@ -5,31 +5,37 @@ import sys.io.File;
 
 class ModuleFactory
 {
-	private static var USAGE : String = "Setup usage: create_module module_name [-path module_path] [-force]";
+	private static var USAGE : String = "Setup usage: create_module module_name [-path module_path] [-force] [-package packageName]";
     private static var MODULE_TEMPLATE_DIR : String = "moduleTemplate";
 
 	private static var moduleName : String;
+    private static var packageName : String;
 	private static var modulePath : String = "beluga\\module\\";
-	private static var isDefaultPath = true;
     private static var isForce = false;
 
 	private static function checkArgs(userArgs : Array<String>) : Bool {
 		var isNextModulePath = false;
+        var isNextPackageName = false;
 
 		for (arg in userArgs) {
 			if (isNextModulePath) {
 				isNextModulePath = false;
 				modulePath = arg;
-                isDefaultPath = false;
 			}
+            if (isNextPackageName) {
+                isNextPackageName = false;
+                packageName = arg;
+            }
 			else switch (arg) {
+                case "-package":
+                    isNextPackageName = true;
 				case "-path":
 					isNextModulePath = true;
                 case "-force":
                     isForce = true;
 				default:
 					if (moduleName == null) {
-						moduleName = arg;
+						moduleName = arg.charAt(0).toUpperCase() + arg.substring(1);
 					}
 					else {
 						Sys.println("Warning: Extra or invalid parameter \"" + arg + "\" will be ignored.");
@@ -57,32 +63,31 @@ class ModuleFactory
         }
     }
 
-    private static function replaceTemplate(path: String, moduleName: String) {
+    private static function replaceTemplate(path: String) {
         var content = File.getContent(path);
         var template = new haxe.Template(content);
         var output = template.execute({
-            className: moduleName.charAt(0).toUpperCase() + moduleName.substring(1),
-            moduleName: moduleName.charAt(0).toLowerCase() + moduleName.substring(1)}
-        );
+            className: moduleName,
+            packageName: packageName,
+            helloWordMsg: "::helloWordMsg::"
+        });
         FileSystem.deleteFile(path);
         //rename mtt file to corresponding hx file. +7 is to skip "module" word.
-        File.saveContent(path.substr(0, path.lastIndexOf("\\")) + "\\" + moduleName +
-                         path.substr(path.lastIndexOf("\\") + 7).split(".mtt")[0], output);
+        var newName = moduleName + path.substr(path.lastIndexOf("\\") + 7);
+        newName = newName.substr(0, newName.lastIndexOf(".mtt"));
+        File.saveContent(path.substr(0, path.lastIndexOf("\\")) + "\\" + newName, output);
     }
 
-    private static function recurseReplaceTemplates(currentFolder: String, moduleName: String) {
+    private static function recurseReplaceTemplates(currentFolder: String) {
         for (item in FileSystem.readDirectory(currentFolder)) {
             var path = currentFolder + "\\" + item;
             if (FileSystem.exists(path)) {
                 //recurse on any directoy
                 if (FileSystem.isDirectory(path))
-                    recurseReplaceTemplates(path, moduleName);
+                    recurseReplaceTemplates(path);
                 //replace any template file
                 else if (item.lastIndexOf(".mtt") != -1)
-                    replaceTemplate(path, moduleName);
-            } else {
-
-                Sys.println("something's wrong: " + path);
+                    replaceTemplate(path);
             }
         }
     }
@@ -90,7 +95,8 @@ class ModuleFactory
 	public static function run(libDir : String, userArgs : Array<String>) : String
 	{
 		if (checkArgs(userArgs)) {
-            var fullModulePath = (isDefaultPath ? libDir : "") + modulePath + moduleName;
+            packageName = packageName != null ? packageName : moduleName.toLowerCase();
+            var fullModulePath = modulePath + packageName;
 
 			Sys.println("Creating module \"" + moduleName + "\" in the folder:\n\"" + fullModulePath + "\"\n");
 
@@ -100,14 +106,15 @@ class ModuleFactory
                 else
 				    return "the folder " + fullModulePath + " already exists";
 			} else {
-                FileSystem.createDirectory(fullModulePath + "/api");
+                FileSystem.createDirectory(fullModulePath);
             }
 
             //Copy the whole module directory
+            Sys.println(fullModulePath);
             cpDirectory(MODULE_TEMPLATE_DIR, fullModulePath);
 
             //Remplace and fill all template files
-            recurseReplaceTemplates(fullModulePath, moduleName);
+            recurseReplaceTemplates(fullModulePath);
 			Sys.println("\nDone");
 			return null;
 		}
